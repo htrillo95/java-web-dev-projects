54d87e59d1a234a4be6061cac89fc0ac473d8e5b
Hector Trillo <htrillo95@gmail.com>
2024-04-09 21:18:26 -0400

    update

--- classes-part-2/exercises/src/main/java/org/launchcode/Course.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Course.java
@@ -14,4 +14,66 @@ public class Course {
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
 
+    public Course(String topic, Teacher instructor) {
+        this.topic = topic;
+        this.instructor = instructor;
+        this.enrolledStudents = new ArrayList<>();
+    }
+
+    // Custom toString method
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Course: ").append(topic).append("\n");
+        stringBuilder.append("Instructor: ").append(instructor).append("\n");
+        stringBuilder.append("Enrolled Students: \n");
+        for (Student student : enrolledStudents) {
+            stringBuilder.append(student).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    // Custom equals method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Course course = (Course) obj;
+        return topic.equals(course.topic) &&
+                instructor.equals(course.instructor) &&
+                enrolledStudents.equals(course.enrolledStudents);
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Teacher getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Teacher instructor) {
+        this.instructor = instructor;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+    public void addStudent(Student student) {
+        enrolledStudents.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        enrolledStudents.remove(student);
+    }
+
 }
